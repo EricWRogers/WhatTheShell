30aa7ea48f85c9b9985ea499aff219aad15f96d2
Eric <yerick147083@gmail.com>
2018-09-25 22:55:54 -0500

    Start on AI

---
+++ Assets/Scripts/AI/MushroomMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomMovement : MonoBehaviour {
+
+	public LayerMask enemyMask;
+	public float speed;
+	Rigidbody2D myBody;
+	Transform myTrans;
+	float myWidth;
+	
+	// Use this for initialization
+	void Start () {
+		myTrans = this.transform;
+		myBody = the.GetComponent<Rigidbody2D>();
+		myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+		//Debug.DrawLine();
+
+		//Always move forward
+		Vector2 myVel = myBody.velocity;
+		myVel.x = speed;
+		myBody.velocity = myVel;
+	}
+}
