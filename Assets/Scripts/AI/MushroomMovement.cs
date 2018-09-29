94da216af701dfcf7be8f7ded7c74367f9d56f16
Eric <yerick147083@gmail.com>
2018-09-29 17:54:01 -0500

    AI is no longer killing it self

--- Assets/Scripts/AI/MushroomMovement.cs
+++ Assets/Scripts/AI/MushroomMovement.cs
@@ -6,6 +6,8 @@ public class MushroomMovement : MonoBehaviour {
 
 	public LayerMask enemyMask;
 	public float speed;
+	[SerializeField]
+	private bool isGrounded;
 	Rigidbody2D myBody;
 	Transform myTrans;
 	float myWidth;
@@ -13,15 +15,23 @@ public class MushroomMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		myTrans = this.transform;
-		myBody = the.GetComponent<Rigidbody2D>();
+		myBody = this.GetComponent<Rigidbody2D>();
 		myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
-		//Debug.DrawLine();
+		Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+		isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down);
 
+		if(!isGrounded)
+		{
+			Vector3 currRot = myTrans.eulerAngles;
+			currRot.y +=  180;
+			myTrans.eulerAngles = currRot;
+			speed = speed * -1;
+		}
 		//Always move forward
 		Vector2 myVel = myBody.velocity;
 		myVel.x = speed;
