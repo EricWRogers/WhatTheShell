664436c2bc3ecac6b88a617f78eb07ee78c28c35
D'Raven McCoy <dravenmccoy1@gmail.com>
2018-10-05 16:03:57 -0500

    Made a basic start menu that closes the game, but does not open anything because ther is no game to open to yet.

---
+++ Assets/StartMenuCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuCode : MonoBehaviour
+{
+
+    public void OpenGame()
+    {
+        Debug.Log("Game Machine On.");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Machine Broke.");
+    }
+	
+}
